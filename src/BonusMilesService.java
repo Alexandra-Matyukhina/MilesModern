9e9af423e97e8c219403b7811e369f8e7225e1db
Alexandra Matyukhina <alexandra.paramygina@gmail.com>
2021-05-28 09:43:16 +0300

    Modified src/

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,10 +1,9 @@
 public class BonusMilesService {
-    public int calculate(int cost){
+    public int calculate(int cost) {
 
-        int mile = 1; // бонус в милях
         int mileBonus = 20; // бонус за каждые 20 рублей
 
-        int totalMiles = ((cost / mileBonus) * mile); // всего милей от стоимости билета
+        int totalMiles = (cost / mileBonus); // всего милей от стоимости билета
         return totalMiles;
 
     }
